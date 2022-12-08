83599e9d25c7f948dd21b00a61ab4ac17b97488b
Francken Louis <220153@CCM-Asty-Moulin.local>
2022-12-08 16:17:36 +0100

    Fin

---
+++ Ce1UAA11_2022_FranckenLouis/CODE OFFICIEL/Ce1UAA11_2022_FranckenLouis/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+
+namespace Ce1UAA11_2022_FranckenLouis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double leave;
+            leave = 0;
+            double nbrlancer;
+            string totalU1;
+            string totalU2;
+            string resultatU1;
+            string resultatU2;
+            double verif;
+            verif = 0;
+            totalU1 = "";
+            totalU2 = "";
+            resultatU1 = "";
+            resultatU2 = "";
+            do
+            {
+                Console.WriteLine("ZANZIBAR!");
+
+                do
+                {
+                    Console.Write("Combien de lancers désirez-vous? (Entre 3 & 9");
+                    nbrlancer = double.Parse(Console.ReadLine());
+                } while (nbrlancer > 9 || nbrlancer < 3);
+
+                generator(nbrlancer, totalU1, totalU2, resultatU1, resultatU2, verif);
+
+                do
+                {
+                    Console.Write("Souhaitez-vous recommencer?");
+                    leave = double.Parse(Console.ReadLine());
+                } while (leave != 0 || leave != 1);
+
+            } while (leave == 0);
+
+            Console.WriteLine("Merci d'avoir passé un moment avec nous!");
+
+
+
+
+
+        }
+
+
+        static void calculator(double temporaire, double player, out string totalU2, out string totalU1)
+        {
+            totalU1 = "";
+            totalU2 = "";
+
+            if (temporaire == 1)
+            {
+                if (player == 1)
+                {
+                    totalU1 = totalU1 + 100;
+                } else
+                {
+                    totalU2 = totalU2 + 100;
+                }
+            } else
+            {
+                if (temporaire == 2)
+                {
+                    if (player == 1)
+                    {
+                        totalU1 = totalU1 + 2;
+                    }
+                    else
+                    {
+                        totalU2 = totalU2 + 2;
+                    }
+                }
+                else
+                {
+                    if (temporaire == 3)
+                    {
+                        if (player == 1)
+                        {
+                            totalU1 = totalU1 + 3;
+                        }
+                        else
+                        {
+                            totalU2 = totalU2 + 3;
+                        }
+                    }
+                    else
+                    {
+                        if (temporaire == 4)
+                        {
+                            if (player == 1)
+                            {
+                                totalU1 = totalU1 + 4;
+                            }
+                            else
+                            {
+                                totalU2 = totalU2 + 4;
+                            }
+                        }
+                        else
+                        {
+                            if (temporaire == 5)
+                            {
+                                if (player == 1)
+                                {
+                                    totalU1 = totalU1 + 5;
+                                }
+                                else
+                                {
+                                    totalU2 = totalU2 + 5;
+                                }
+                            }
+                            else
+                            {
+                                if (temporaire == 6)
+                                {
+                                    if (player == 1)
+                                    {
+                                        totalU1 = totalU1 + 60;
+                                    }
+                                    else
+                                    {
+                                        totalU2 = totalU2 + 60;
+                                    }
+                                }
+                                else
+                                {
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
+        static void generator(double nbrlancer, string totalU1, string totalU2, string resultatU1, string resultatU2, double verif)
+        {
+            double i;
+            double temporaire;
+            double sousverif1;
+            double sousverif2;
+            double player;
+            sousverif1 = 0;
+            sousverif2 = 0;
+            Random alea = new Random();
+
+            i = 1;
+            temporaire = 0;
+
+
+            do
+            {
+                for (int j = 1; j != 3; j++ )
+                {
+                    temporaire = alea.Next(1, 7);
+                    resultatU1 = resultatU1 + temporaire + " ";
+                    player = 1;
+                    calculator(temporaire, player, out totalU2, out totalU1);
+                    if (temporaire == 2)
+                    {
+                        sousverif1 = sousverif1 + 1;
+                    } else
+                    {
+                        sousverif2 = sousverif2 + 1;
+                    }
+                    temporaire = alea.Next(1, 7);
+                    resultatU2 = resultatU2 + temporaire + " ";
+                    player = 2;
+                    calculator(temporaire, player, out totalU2, out totalU1);
+                    if (temporaire == 2)
+                    {
+                        sousverif1 = sousverif1 + 1;
+                    }
+                    else
+                    {
+                        sousverif2 = sousverif2 + 1;
+                    }
+                }
+                resultatU1 = resultatU1 + "- ";
+                resultatU2 = resultatU2 + "- ";
+                if (sousverif1 == 3 || sousverif2 == 3)
+                {
+                    verif = 1;
+                } else
+                {
+                    sousverif2 = 2;
+                }
+            } while (i != nbrlancer || verif != 1);
+        }
+    }
+}
---
+++ Ce1UAA11_2022_FranckenLouis/Ce1UAA11_2022_FranckenLouis (Brouillon)/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Ce1UAA11_2022_FranckenLouis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool arret;
+            arret = false;
+            int points;
+            points = 0;
+            double nbrtour;
+
+
+            
+
+            Console.WriteLine("ZANZIBAR !");
+            do
+            {
+                Console.WriteLine("Combien de lancers désirez-vous? (entre 3 et 9) ");
+                nbrtour = double.Parse(Console.ReadLine());
+
+
+                for (int a = 1; a != nbrtour; a++)
+                {
+                    tourDeJeu(out string desSortis, out int pointsTour, out arret, out points);
+                }
+                
+            } while (arret != true);
+
+        }
+
+
+        static void comptePoints(int de, out int points)
+        {
+            if (de == 1)
+            {
+                points = 100;
+            } else
+            {
+                if (de >= 2 && de <= 5)
+                {
+                    points = de;
+                } else
+                {
+                    points = de;
+                }
+            }
+        }
+
+        static void tourDeJeu(out string desSortis, out int pointsTour, out bool arret, out int points)
+        {
+            int compte2;
+            int de;
+            Random alea = new Random();
+            points = 0;
+
+            desSortis = "";
+            pointsTour = 0;
+            arret = false;
+            compte2 = 0;
+
+            for (int i = 1; i <= 4; i++ )
+            {
+                de = alea.Next(1, 7);
+                if (de == 2)
+                {
+                    compte2 = compte2 + 1;
+                };
+                desSortis = desSortis + de + " ";
+                comptePoints(de, out points);
+                pointsTour = pointsTour + points;
+            }
+            if (compte2 == 3)
+            {
+                arret = true;
+            }
+            
+        }
+
+       
+    }
+}
