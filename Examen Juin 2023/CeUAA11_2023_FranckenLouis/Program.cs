b30c98a463c22590fa7f7b0dcab533b2a5ac1fb9
lo-franckenstein <220153@site.asty-moulin.be>
2023-06-01 16:16:10 +0200

    Exam

---
+++ Examen Juin 2023/CeUAA11_2023_FranckenLouis/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Security;
+using System.Security.Cryptography;
+
+namespace CeUAA11_2023_FranckenLouis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            appel mp = new appel();
+            ConsoleKeyInfo keyLetter;
+            int crypt;
+            string PhClaire;
+            string phClef;
+            string phrase;
+            string[,] matrice;
+            bool leave;
+            bool verif;
+            verif = false;
+            crypt = 0;
+            string question;
+            int a;
+            int b;
+            a = 0;
+            b = 0;
+            string convertisseur;
+
+            do
+            {
+                do
+                {
+                    Console.WriteLine("CRYPTAGE\n========\n\nChoisissez parmi les options suivantes :\n1 - Cryptage de Vigenère\n2 - Cryptage avec la méthode affine");
+                    keyLetter = Console.ReadKey();
+                    Console.WriteLine("\n");
+                } while (keyLetter.Key != ConsoleKey.NumPad1 && keyLetter.Key != ConsoleKey.D1 && keyLetter.Key != ConsoleKey.NumPad2 && keyLetter.Key != ConsoleKey.D2);
+
+                if (keyLetter.Key == ConsoleKey.NumPad1 || keyLetter.Key == ConsoleKey.D1)
+                {
+                    crypt = 1;
+                }
+                else if (keyLetter.Key == ConsoleKey.NumPad2 || keyLetter.Key == ConsoleKey.D2)
+                {
+                    crypt = 2;
+                }
+
+                Console.WriteLine("Encodez la phrase à crypter !");
+                PhClaire = Console.ReadLine();
+                phrase = PhClaire;
+                mp.CryptPhrase(ref phrase);
+                PhClaire = phrase;
+
+                Console.WriteLine("");
+                if (crypt == 1)
+                {
+                    Console.WriteLine("Vous avez choisi la méthode de Vigenère !\n-----------------------------------------\nEncodez ma cmé à utiliser dans la méthode de Vigenère !");
+                    phClef = Console.ReadLine();
+                    phrase = phClef;
+                    mp.CryptPhrase(ref phrase);
+                    phClef = phrase;
+                    mp.CryptVigenere(PhClaire, phClef, out matrice);
+                    mp.CryptConv(matrice, out convertisseur);
+                    Console.WriteLine(convertisseur);
+
+                }
+                else
+                {
+                    Console.WriteLine("Vous avez choisi la méthode de cryptage affine !\n-----------------------------------------");
+                    do
+                    {
+                        question = "Donnez la valeur du coefficient de (impair entre 1 et 25 sauf 13) :";
+                        mp.LireReel(question, out a);
+                        if (a == 1 || a == 3 || a == 5 || a == 7 || a == 9 || a == 11 || a == 15 || a == 17 || a == 19 || a == 21 || a == 23 || a == 25)
+                        {
+                            verif = true;
+                        }
+                        else
+                        {
+                            verif = false;
+                        }
+                    } while (verif == false);
+                    do
+                    {
+                        question = "Donnez la valeur du coefficient b :";
+                        mp.LireReel(question, out b);
+                        if (b >= 0 && b <= 25)
+                        {
+                            verif = true;
+                        }
+                        else
+                        {
+                            verif = false;
+                        }
+                    } while (verif == false);
+                    mp.CryptAffine(PhClaire, a, b, out matrice);
+                    mp.CryptConv(matrice, out convertisseur);
+                    Console.WriteLine(convertisseur);
+                }
+
+                Console.WriteLine("Un autre cryptage ? o = oui / autre = non");
+                keyLetter = Console.ReadKey();
+                leave = true;
+                if (keyLetter.Key == ConsoleKey.O)
+                {
+                    leave = false;
+                }
+            } while (leave == false);
+            
+
+
+        }
+    }
+}
---
+++ Examen Juin 2023/CeUAA11_2023_FranckenLouis/appel.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace CeUAA11_2023_FranckenLouis
+{
+
+
+
+
+
+
+    public struct appel
+    {
+
+
+        public void CryptAffine(string PhClaire, int a, int b, out string[,] matAffine)
+        {
+            int x;
+            int y;
+            matAffine = new string[4, PhClaire.Length];
+            for (int i = 0; i < PhClaire.Length - 2; i++)
+            {
+                matAffine[0, i] = PhClaire[i].ToString();
+                x = ((int)PhClaire[i] - 65);
+                matAffine[1, i] = x.ToString();
+                y = (a * x + b) % 26;
+                matAffine[2, i] = y.ToString();
+                matAffine[3, i] = (y + 65).ToString();
+
+            }
+        }
+
+
+
+
+        public void CryptVigenere(string PhClaire, string phClef, out string[,] matViegenere)
+        {
+            int j;
+            int codeAscii;
+
+            matViegenere = new string[4, PhClaire.Length];
+            j = 0;
+            for (int i = 0; i < PhClaire.Length; i++)
+            {
+                matViegenere[0, i] = PhClaire[i].ToString();
+                if (j == phClef.Length)
+                {
+                    j = 0;
+                }
+                matViegenere[1, i] = PhClaire[j].ToString();
+                matViegenere[2, i] = (((int)phClef[j]) - 65).ToString();
+                if (((int)PhClaire[i]) + int.Parse(matViegenere[2, i]) <= 90)
+                {
+                    codeAscii = (int)char.Parse(matViegenere[0, i]) + int.Parse(matViegenere[2, i]);
+                }
+                else
+                {
+                    codeAscii = (int)char.Parse(matViegenere[0, i]) + int.Parse(matViegenere[2, i]) - 26;
+                }
+                matViegenere[3, i] = Convert.ToChar(codeAscii).ToString();
+                j++;
+
+            }
+
+        }
+
+        public void CryptPhrase(ref string phrase)
+        {
+            int number;
+            string[] stock;
+            string NPhrase;
+            number = phrase.Length;
+            stock = new string[number];
+            NPhrase = "";
+
+            for (int i = 0; i < number - 1; i++)
+            {
+                stock[i] = phrase[i].ToString();
+            }
+
+            for (int i = 0; i < number - 1; i++)
+            {
+                if (stock[i] == "-" || stock[i] == " " || stock[i] == "'" || stock[i] == "_" || stock[i] == "")
+                {
+                    NPhrase = "";
+                } else
+                {
+                    NPhrase = stock[i];
+                }
+            }
+
+            phrase = NPhrase;
+
+            
+
+        }
+
+
+        public void CryptConv(string[,] matrice, out string convertisseur)
+        {
+            int rows;
+            int column;
+            rows = matrice.GetLength(0);
+            column = matrice.GetLength(1);
+            convertisseur = "Voici la matrice de cryptage : \n------------------------------\n";
+            for(int i = 0;i < rows - 1;i++)
+            {
+                for (int j = 0; j < column - 1; j++)
+                {
+                    convertisseur += matrice[i, j] + " ";
+                }
+                convertisseur += "\n";
+            }
+            convertisseur += "\n\nRésultat du cryptage par la méthode de Vigenère\n";
+            for (int i = 0; i < rows - 1; i++)
+            {
+                convertisseur += matrice[4, i];
+            }
+
+        }
+
+
+        public void LireReel(string question, out int n)
+        {
+            string nUser;
+            Console.Write(question);
+            nUser = Console.ReadLine();
+            while (!int.TryParse(nUser, out n))
+            {
+                Console.WriteLine(question);
+                nUser = Console.ReadLine();
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
